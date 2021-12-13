fc0592cbd3f6b8d223fc95e118c629e33a986a46
Jayce Nguyen <32528654+ShorthandedShot@users.noreply.github.com>
2021-12-13 14:39:22 -0500

    lazyload v2
    fixed lazy loading............ kinda

--- functions.js
+++ functions.js
@@ -108,36 +108,36 @@ $(window).scroll(function() {
 if ($(this).scrollTop()>240) {
 $('.topBar').slideDown( 200, "linear");
 } else {
-$('.topBar').fadeOut(300, "linear");
+$('.topBar').fadeOut(200, "linear");
 }
 });
 
 // GRID RESIZE....
 
-function resizeGridItem(item){
-  grid = document.getElementsByClassName("grid")[0];
-  rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-  rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-  rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
-    item.style.gridRowEnd = "span "+rowSpan;
-}
-
-function resizeAllGridItems(){
-  allItems = document.getElementsByClassName("item");
-  for(x=0;x<allItems.length;x++){
-    resizeGridItem(allItems[x]);
-  }
-}
-
-function resizeInstance(instance){
-	item = instance.elements[0];
-  resizeGridItem(item);
-}
-
-window.onload = resizeAllGridItems();
-window.addEventListener("resize", resizeAllGridItems);
-
-allItems = document.getElementsByClassName("item");
-for(x=0;x<allItems.length;x++){
-  imagesLoaded( allItems[x], resizeInstance);
-}
+// function resizeGridItem(item){
+//   grid = document.getElementsByClassName("grid")[0];
+//   rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+//   rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+//   rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+//     item.style.gridRowEnd = "span "+rowSpan;
+// }
+//
+// function resizeAllGridItems(){
+//   allItems = document.getElementsByClassName("item");
+//   for(x=0;x<allItems.length;x++){
+//     resizeGridItem(allItems[x]);
+//   }
+// }
+//
+// function resizeInstance(instance){
+// 	item = instance.elements[0];
+//   resizeGridItem(item);
+// }
+//
+// window.onload = resizeAllGridItems();
+// window.addEventListener("resize", resizeAllGridItems);
+//
+// allItems = document.getElementsByClassName("item");
+// for(x=0;x<allItems.length;x++){
+//   imagesLoaded( allItems[x], resizeInstance);
+// }
